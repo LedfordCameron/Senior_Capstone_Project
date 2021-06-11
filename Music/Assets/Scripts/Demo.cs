a887c17a3e74ce80104211c96e543fe11e8aea5b
LedfordCameron <Ledford_Cameron@columbusstate.edu>
2021-06-11 18:25:21 -0400

    Added Demo
    I added a demo to explain static

---
+++ Music/Assets/Scripts/Demo.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Demo : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private int score;
+    public Text txtScore;
+    void Start()
+    {
+        score = 0;
+    }
+
+    public void AddScore(){
+        score++;
+        txtScore.text = score.ToString();
+    }
+
+    public void GoToMainMenuScene(){
+        SceneManager.LoadScene("Main_Menu");    
+    }
+}
